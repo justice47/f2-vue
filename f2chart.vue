5c3c68ccac190a82a8b7954be9f03021b4f45243
justice47 <yaroslav.ahromeev@ite-exhibitions.com>
2019-10-23 12:37:10 +0300

    added basic f2-vue functionality

---
+++ f2chart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <canvas id="mountNode"></canvas>
+  </div>
+</template>
+
+<script>
+import F2 from "@antv/f2";
+
+export default {
+  name: "f2chart",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    geometry: {
+      required: true
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    const chart = new F2.Chart({
+      id: "mountNode",
+    })
+    chart.source(this.data);
+    for (let i = 0; i < this.geometry.length; i++) {
+      chart[this.geometry[i].type]()
+        .position(
+          this.geometry[i].position[0] + "*" + this.geometry[i].position[1]
+        )
+    }
+
+    chart.render();
+  }
+}
+</script>
